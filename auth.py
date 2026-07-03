c4a83081ddd4e4bc1fd1d41cc331273d5d120862
Wangui <138119862+kui1k@users.noreply.github.com>
2026-07-03 00:19:12 -0700

    updated auth and set up database

--- auth.py
+++ auth.py
@@ -9,6 +9,14 @@ import os
 from passlib.context import CryptContext
 from datetime import datetime,timedelta
 from jose import  JWTError, jwt
+from dotenv import load_dotenv
+from fastapi import HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer
+from database import get_db
+
+
+Oauth2_scheme=OAuth2PasswordBearer(tokenUrl="/login")
+load_dotenv()
 
 password_context=CryptContext(["bcrypt"])
 
@@ -36,5 +44,13 @@ def verify_token(token:str):
     except JWTError:
         return None
     
-
-
+def get_current_user(token:str=Depends(Oauth2_scheme),db=Depends(get_db)):
+    username=verify_token(token)
+    if username is None:
+        raise HTTPException(status_code=401,detail="unauthorized user")
+    user=db.query(User).filter(User.name == username).first()
+    if user is None:
+        raise HTTPException(status_code=401,detail="unauthorized user")
+    return user
+    
+    #import user from models after creating models
--- database.py
+++ database.py
@@ -1,3 +1,27 @@
 #location
 #connection
 #conversation
+#foundation
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+database_location= os.getenv("DATABASEURL")
+
+engine=create_engine(database_location,connect_args="check_same_thread":False)
+#connect_ags agument is specific to sqlite
+
+SessionLocal=sessionmaker(bind=engine, autoflush=False, autocommit=False)
+Base= declarative_base()
+
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
